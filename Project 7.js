9ddbfca5b2747716ff320483216bad2d715e5429
Gaurav Pandey <61927226+gauravgap@users.noreply.github.com>
2020-07-07 21:16:42 +0530

    Add files via upload

---
+++ Project 7.js
@@ -0,0 +1,8 @@
+var myAge = 20;
+var firstYears = 2 * 10.5;
+var laterYears = (myAge - 2) * 4;
+var myAgeInDogYears = firstYears + laterYears;
+console.log('My age in dog years is ' +myAgeInDogYears + '.');
+var myName = 'Gaurav Pandey'.toLowerCase();
+console.log(`My name is ${myName}.
+     I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
