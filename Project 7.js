58ffa81fc465d376dc3501dcdea28b07f7c25c68
Gaurav Pandey <61927226+gauravgap@users.noreply.github.com>
2020-07-08 20:51:57 +0530

    Update Project 7.js

--- Project 7.js
+++ Project 7.js
@@ -1,8 +1,15 @@
 var myAge = 20;
-var firstYears = 2 * 10.5;
+// age is assigned to myAge variable
+var earlyYears = 2 * 10.5;
+// new value is assigned to earlyYears variable
 var laterYears = (myAge - 2) * 4;
-var myAgeInDogYears = firstYears + laterYears;
+// using arithmetic operators and saving the value to laterYears variable
+var myAgeInDogYears = earlyYears + laterYears;
+// addition of earlyYears and laterYears is saved to myAgeinDogYears variable
 console.log('My age in dog years is ' +myAgeInDogYears + '.');
 var myName = 'Gaurav Pandey'.toLowerCase();
+// here The toLowerCase method returns a string with all lowercase letters.
 console.log(`My name is ${myName}.
      I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+ // here  NAME is replaced with myName, HUMAN AGE with myAge, and DOG AGE with myAgeInDogYears in the sentence above.    
+
